39f342f4cb3e47e37d2ef141fbc66025b002de01
bshaerk <b34777692@gmail.com>
2026-01-19 21:24:26 +0700

    A new beginning

--- main.lua
+++ main.lua
@@ -1,1 +1,30 @@
-Hello, World!
+runtime.version = "Lua 5.3"
+local lgi = require 'lgi'
+local Gtk = lgi.require("Gtk", "3.0")
+
+local window = Gtk.Window {
+  title = "Notes",
+  Gtk.Box {
+    orientation = 'VERTICAL',
+    Gtk.Entry {
+      id = 'entry'
+    },
+    Gtk.Box {
+      orientation = 'HORIZONTAL',
+      Gtk.Button {
+        label = 'Save',
+        id = 'save'
+      },
+      Gtk. Button {
+        label = 'Open',
+        id = 'open'
+      }
+    }
+  },
+  on_destroy = function()
+    Gtk.main_quit()
+  end
+}
+
+window:show_all()
+Gtk.main()
